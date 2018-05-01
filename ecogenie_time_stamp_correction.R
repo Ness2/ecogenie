1ea4eb485e0eecfdb8d910412942ceaab570175c
Ness 2 <37035005+Ness2@users.noreply.github.com>
2018-05-01 15:42:03 +0200

    Delete ecogenie_time_stamp_correction.R

--- ecogenie_time_stamp_correction.R
+++
@@ -1,8 +0,0 @@
-#####################################################################################
-#
-# ecogenie Time Stamp Correction
-#
-#####################################################################################
-
-# Time Stamp Correction
-ecogenie_datafile[,1] <- ecogenie_datafile[,1] %>% as.POSIXct(format = "%d.%m.%Y:%T", tz = "GMT")
